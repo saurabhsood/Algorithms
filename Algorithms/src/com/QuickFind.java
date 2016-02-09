ccb037b876a91fe11a31f66b768dc65985dbdb60
Saurabh Sood <ssoodindia@gmail.com>
2016-02-09 23:34:38 +0530

    fixed

--- Algorithms/src/com/QuickFind.java
+++ Algorithms/src/com/QuickFind.java
@@ -3,7 +3,6 @@ package com;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class QuickFind {
 
@@ -52,6 +51,7 @@ public class QuickFind {
 	 {
 		 return id[p] == id[q];		 
 	 }
+	 
 	 
 	 public static void main(String s[]) throws NumberFormatException, IOException
 	 {
