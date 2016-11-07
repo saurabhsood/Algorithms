e1bf921936d1a174bc58e17055b94efeb5d59a33
Saurabh Sood <ssoodindia@gmail.com>
2016-11-08 00:15:52 +0530

    test

--- Algorithms/src/com/QuickSort.java
+++ Algorithms/src/com/QuickSort.java
@@ -4,8 +4,43 @@ public class QuickSort {
 	
 	public static void main(String s[])
 	{
+		int [] a={2,5,10,1,7,8,3,4,9,6};
+		quick(a);
+	}
+	
+	
+	
+	protected static void quick(int [] a)
+	{
+		
+		int pivot = a[a.length-1];
+		int j =0;
+		for(int i =0 ; i < a.length ; i++)
+		{
+			if(a[i] <= pivot)
+			{
+				if(i==j)
+					j++;
+				else
+				{
+					int ab = a[j];
+					a[j] = a[i];
+					a[i]=ab;
+					j++;
+				}
+			}
+			
+		}
+		System.out.println("sss"+a[j-1]);
+		for(int i =0; i < a.length;i++){
+			System.out.println(a[i]);
+		}
 		
 	}
-
+	
+	protected void divide( int [] a)
+	{
+		
+	}
 	
 }
