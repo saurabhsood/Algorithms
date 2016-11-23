b68dd842f12214e90566435a061d3870e7ecfdc3
Saurabh Sood <ssoodindia@gmail.com>
2016-11-23 23:21:51 +0530

    test

--- Algorithms/src/com/QuickSort.java
+++ Algorithms/src/com/QuickSort.java
@@ -1,46 +1,36 @@
 package com;
 
 public class QuickSort {
-	
-	public static void main(String s[])
-	{
-		int [] a={2,5,10,1,7,8,3,4,9,6};
-		quick(a);
-	}
-	
-	
-	
-	protected static void quick(int [] a)
-	{
-		
-		int pivot = a[a.length-1];
-		int j =0;
-		for(int i =0 ; i < a.length ; i++)
-		{
-			if(a[i] <= pivot)
-			{
-				if(i==j)
-					j++;
-				else
-				{
-					int ab = a[j];
-					a[j] = a[i];
-					a[i]=ab;
-					j++;
-				}
-			}
-			
-		}
-		System.out.println("sss"+a[j-1]);
-		for(int i =0; i < a.length;i++){
+
+	public static void main(String s[]) {
+		int[] a = { 200, 5, 10, 8, 7,1,100};
+		sortOnDivNumber(a, 0, a.length);
+		for (int i = 0; i < a.length; i++) {
 			System.out.println(a[i]);
 		}
+	}
+
+	public static void sortOnDivNumber(int[] a, int start, int end) {
+		if(end-start <=1)
+		{
+			return;
+		}
+		int b = a[end - 1];
+		int counter = start;
+
+		for (int i = start; i < end; i++) {
+			if (a[i] <= b) {
+				if (i != counter) {
+					int hold = a[counter];
+					a[counter] = a[i];
+					a[i] = hold;
+				}
+				counter++;
+			}
+		}
+		sortOnDivNumber(a,start,counter-1);
+		sortOnDivNumber(a,start+counter-1,end);
 		
 	}
-	
-	protected void divide( int [] a)
-	{
-		
-	}
-	
+
 }
